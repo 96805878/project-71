8e24a3e6035c00310e6d63c2a8eb49c041edf03b
96805878 <92256586+96805878@users.noreply.github.com>
2022-11-08 20:23:37 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,16 @@
+import firebase from "firebase";
+require("@firebase/firestore");
+
+//Paste your firebaseConfig here
+const firebaseConfig = {
+    apiKey: "AIzaSyD8XPxAmL3lIAWxGII685FnTwrOT0tQ-po",
+    authDomain: "project-71-10811.firebaseapp.com",
+    projectId: "project-71-10811",
+    storageBucket: "project-71-10811.appspot.com",
+    messagingSenderId: "510082638234",
+    appId: "1:510082638234:web:0c0f52d60a2924149a1a23"
+  };
+//
+firebase.initializeApp(firebaseConfig);
+
+export default firebase.firestore();
